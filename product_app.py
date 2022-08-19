41a665a2083260efb704e80c20a4e72d1ccaabf8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-08-19 16:14:42 +0200

    Update product_app.py

--- product_app.py
+++ product_app.py
@@ -8,6 +8,16 @@ class Product:
         self.wind = window
         self.wind.title('products Application')
 
+        #CREATING A FRAME CONTAINER
+
+        frame = LabelFrame(self.wind, text = 'Register a new Product')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        #name input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 1, column = 1)
+
 if __name__ == '__main__':
     window = Tk()
     application = Product(window)
