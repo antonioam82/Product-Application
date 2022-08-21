4ebe112c1e14bd6df82079e68ce0ed0d1b264525
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-08-21 15:29:38 +0200

    Update product_app.py

--- product_app.py
+++ product_app.py
@@ -4,6 +4,8 @@ import sqlite3
 
 class Product:
 
+    db_name = 'database.db'
+
     def __init__(self,window):
         self.wind = window
         self.wind.title('products Application')
@@ -30,6 +32,22 @@ class Product:
         #table
         self.tree = ttk.Treeview(height = 10, columns = 2)
         self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Name', anchor = CENTER)
+        self.tree.heading('#1', text = 'Price', anchor = CENTER)
+
+        self.get_products()
+
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_products(self):
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        print(db_rows)
 
         
 if __name__ == '__main__':
